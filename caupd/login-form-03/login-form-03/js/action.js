9eac88f7a07b0e7dfb42b09328dc43ea273111b6
browndby366 <browndby@163.com>
2022-11-17 14:09:38 +0800

    d
    d

---
+++ caupd/login-form-03/login-form-03/js/action.js
@@ -0,0 +1,12 @@
+function validate() {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    if (username === 'caupd' && password === 'caupd')
+    {
+        window.open('http://10.10.2.102:5001', '_blank');
+    } else {
+        alert('login 失败')
+    }
+
+}
